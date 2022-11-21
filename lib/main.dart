784288df5d1c93155ae13c51276eca550b61cf5b
Awaizkazi <Owaizkazi789@gmail.com>
2022-11-22 02:23:11 +0530

    OnHold

--- lib/main.dart
+++ lib/main.dart
@@ -27,3 +27,8 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+// ! I have to make an app in Android from Firebase
+// https://www.youtube.com/watch?v=sMA1dKbv33Y&t=2370s
+// Yha se dekna hai pehle App bananeka hai Firebase me
